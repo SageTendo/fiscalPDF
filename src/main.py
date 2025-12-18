96ca3d8461972d09388892fa2060d104c48d25a5
SageTendo <39864316+SageTendo@users.noreply.github.com>
2025-12-18 20:34:07 +0200

    fix arg for web

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src.desktop.app import main as desktop_main
 from src.web.app import main as web_main
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "web":
+    if len(sys.argv) > 1 and sys.argv[1] == "--web":
         web_main()
     else:
         desktop_main()
